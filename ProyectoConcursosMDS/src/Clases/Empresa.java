ff2fe1660969fd0320f9b2392e9f4ab51e44a401
Diego Castillo <Diego Castillo@DESKTOP-V37K5II.mshome.net>
2019-05-28 18:57:44 -0500

    Modificado4

--- ProyectoConcursosMDS/src/Clases/Empresa.java
+++ ProyectoConcursosMDS/src/Clases/Empresa.java
@@ -125,7 +125,7 @@ public class Empresa implements Verificacion,Serializable {
     if(concurso.verificarDatos()){
         for (int i = 0; i < concusos.size(); i++) {
             if(concusos.get(i).getNombre().equals(concurso.getNombre())){
-                JOptionPane.showMessageDialog(null, "Hay un concurso con el mismo nombre,Cambie de nombre a su concurso!","Error",JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Hay un concurso con el mismo nombre,Cambie el nombre a su concurso!","Error",JOptionPane.ERROR_MESSAGE);
                 return false;
               }                
             }
