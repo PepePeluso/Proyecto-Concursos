b331da1e66b51e1612418843667445086ebc65eb
Diego Castillo <Diego Castillo@DESKTOP-V37K5II.mshome.net>
2019-05-28 19:53:17 -0500

    Modificado4

--- ProyectoConcursosMDS/src/Clases/Verificacion.java
+++ ProyectoConcursosMDS/src/Clases/Verificacion.java
@@ -7,9 +7,10 @@ package Clases;
  ***********************************************************************/
 
 import java.util.*;
+import java.io.Serializable;
 
 /** @pdOid 92112c4c-68ce-4a9e-bf57-f82a06a46a54 */
-public interface Verificacion {
+public interface Verificacion  {
    /** @pdOid 746d47ea-eb0f-49a7-99b0-0d476c62ae86 */
    boolean verificarDatos();
 
